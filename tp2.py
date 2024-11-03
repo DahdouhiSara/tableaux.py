d140b00b72dc3928c8c4d89b521190a5a606752f
DahdouhiSara <dahdouhisara25@gmail.com>
2024-11-03 17:14:02 +0100

    tp2.py

---
+++ tp2.py
@@ -0,0 +1,17 @@
+exercice3
+T1=[4,8,7,12]
+T2=[3,6,3,-1]
+T=[]
+i=0
+j=0
+while i <len(T1):
+    if i <len(T1):
+        T.append(T1[i])
+        i=i+1
+while j <len(T2):
+    if j <len(T2[j]):
+        j=j+1
+
+print(T1)
+print(T2)
+print(T)
